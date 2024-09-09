54cc1f50a89674acff8a8a90ecc3c3cc8e212d1e
Adrian <mgallo215@gmail.com>
2024-09-09 00:55:50 -0500

    suffering with the tests

--- tests/app.test.js
+++ tests/app.test.js
@@ -9,14 +9,14 @@ describe('GET /', ()=>{
         server = app.listen(0);
     });
 
-    afterAll(() => {
-        server.close();
-    });
-
     it('should return Hello, World!', async()=>{
         const res = await request(app).get('/');
         expect(res.statusCode).toEqual(200);
         expect(res.text).toBe('Hello, World!');
+    });
+
+    afterAll(() => {
+        server.close();
     });
 });
 
